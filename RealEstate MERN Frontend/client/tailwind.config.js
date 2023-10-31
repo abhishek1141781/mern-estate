3975cb784e53e490992933d3d25d01f0bff8d19c
Abhishek <abhishek1141781@gmail.com>
2023-10-31 14:06:07 +0530

    1st commit react and tailwind

---
+++ RealEstate MERN Frontend/client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// detect a tailwind class and make css file for that
